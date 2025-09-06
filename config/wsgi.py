c15d9053dbeb08dc39a83e9f5f3de472be8ba6d2
fabiane9258 <fabiane9258@gmail.com>
2025-09-06 22:34:15 +0300

    Refactor wsgi.py; clean up comments, streamline project path handling, and ensure correct settings module is specified

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,21 +1,19 @@
-"""
-WSGI config for config project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
-import sys
 import os
+import sys
 
-# Add your project path
-path = '/home/yourusername/project'
-if path not in sys.path:
-    sys.path.append(path)
+# Add your project directory to the sys.path
+project_home = '/home/farbee9258/social-media-api'
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'project.settings'
+# Set the environment variable to tell Django where your settings.py is
+os.environ['DJANGO_SETTINGS_MODULE'] = 'config.settings'  # matches your settings.py inside config folder
 
+# Activate your virtualenv
+activate_this = '/home/farbee9258/.virtualenvs/social-media-api/bin/activate_this.py'
+with open(activate_this) as file_:
+    exec(file_.read(), dict(__file__=activate_this))
+
+# Serve Django via WSGI
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
